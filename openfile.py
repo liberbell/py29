4b52edabc53d2dd0bd0076a2e6755dbd9dc16c1a
Liberbell <liberbell@gmail.com>
2020-11-09 09:21:20 +0900

    openfiles3

--- openfile.py
+++ openfile.py
@@ -1,34 +1,34 @@
-open("data_file/example.txt")
-print(open("data_file/example.txt"))
+# open("data_file/example.txt")
+# print(open("data_file/example.txt"))
 
-file = open("data_file/example.txt")
-print(file)
+# file = open("data_file/example.txt")
+# print(file)
 
-print(file.mode)
-print(file.name)
-print(file.closed)
+# print(file.mode)
+# print(file.name)
+# print(file.closed)
 
-file.close()
-print(file.closed)
+# file.close()
+# print(file.closed)
 
-file = open("data_file/example.txt")
-print(file.read())
+# file = open("data_file/example.txt")
+# print(file.read())
 
-print(file.seek(0))
-print(file.read(5))
-print(file.tell())
-print(file.read(5))
+# print(file.seek(0))
+# print(file.read(5))
+# print(file.tell())
+# print(file.read(5))
 
-print(file.tell())
+# print(file.tell())
 
-file.seek(0)
-print(file.tell())
+# file.seek(0)
+# print(file.tell())
 
-print(file.read(15))
-print(file.tell())
-print(file.read())
+# print(file.read(15))
+# print(file.tell())
+# print(file.read())
 
-file.close()
+# file.close()
 
 with open("data_file/example.txt") as f:
     data = f.readline()
