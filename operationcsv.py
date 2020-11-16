48f420e2e9258a76472b6bcf50991988d26bf1e2
Liberbell <liberbell@gmail.com>
2020-11-16 10:06:53 +0900

    operationcsv5

--- operationcsv.py
+++ operationcsv.py
@@ -33,3 +33,12 @@ with file:
     read = csv.reader(file, delimiter="\t")
     for row in read:
         print(row)
+
+print("--------\n")
+
+file = open("data_file/record.csv", "r")
+
+with file:
+    read = csv.DictReader(file)
+    for row in read:
+        print(dict(row))
