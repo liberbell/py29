bc3d8f80f0612ae14e44817e59ae26035356c4ea
Liberbell <liberbell@gmail.com>
2020-11-17 08:50:17 +0900

    operationcsv2

--- operationcsv.py
+++ operationcsv.py
@@ -56,3 +56,10 @@ names = [["First Name", "Last Name"],
          ["Sofia", "Reyes"],
          ["Jerome", "Jackson"]
          ["Jia", "Zhong"]]
+
+file = open("data_file/names.csv", "w")
+
+with file:
+    file_writer = csv.writer(file)
+    for row in names:
+        file_writer.writerow(row)
