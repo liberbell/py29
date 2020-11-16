f76ea92d0527ae5a567b59120ed9c7a3a9306876
Liberbell <liberbell@gmail.com>
2020-11-16 09:59:22 +0900

    operationcsv3

--- operationcsv.py
+++ operationcsv.py
@@ -6,3 +6,10 @@ with file:
     read = csv.reader(file)
     for row in read:
         print(row)
+
+file = open("data_file/record_pipe.csv", "r")
+
+with file:
+    read = csv.reader(file)
+    for row in read:
+        print(row)
