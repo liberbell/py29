59f2fa461bbf7c1368788d98772ebf5441b196b4
Liberbell <liberbell@gmail.com>
2020-11-16 10:01:22 +0900

    operationcsv4

--- operationcsv.py
+++ operationcsv.py
@@ -7,9 +7,21 @@ with file:
     for row in read:
         print(row)
 
+print("--------\n")
+
 file = open("data_file/record_pipe.csv", "r")
 
 with file:
     read = csv.reader(file)
     for row in read:
         print(row)
+
+print("--------\n")
+
+file = open("data_file/record_pipe.csv", "r")
+
+with file:
+    read = csv.reader(file, delimiter="|")
+    for row in read:
+        print(row)
+
